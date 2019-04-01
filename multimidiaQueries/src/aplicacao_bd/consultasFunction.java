e4d23c6b59ad5c91b8792dd6ac04b0d39a8e8883
FranciscoRodriguesNeto <francisco.rneto@dcomp.ufs.br>
2019-03-31 23:23:14 -0300

    adiciona funcao na classe de consultas

--- multimidiaQueries/src/aplicacao_bd/consultasFunction.java
+++ multimidiaQueries/src/aplicacao_bd/consultasFunction.java
@@ -5,10 +5,20 @@
  */
 package aplicacao_bd;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Francisco Rodrigues
  */
 public class consultasFunction {
+    private static Connection conexao;
+    
+    public static void chamaConexao () throws SQLException, ClassNotFoundException{
+        conexao = Conexao.setConnection("127.0.0.1:1234", "postgres", "postgres", "JtkMPma0");
+    }
     
 }
